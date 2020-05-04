ade034e04a5cf7bf22d964799858dbd536f5d63d
sushovan <48546053+sushovanINDIA@users.noreply.github.com>
2020-05-04 18:10:05 +0530

    Data Fetching with react-bootstrap-loader

--- Getdata.js
+++ Getdata.js
@@ -1,42 +1,91 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
+import axios from 'axios';
+//import BootstrapTable from 'reactjs-bootstrap-table';
+//import Table from 'react-bootstrap/Table'
+import './Getdata.css';
+import { Link } from 'react-router-dom';
+import * as ReactBootStrap from 'react-bootstrap';
+import Footer from './Footer';
 import 'tachyons';
-import {Link} from 'react-router-dom';
-import './SideMenu.css';
 
-class SideMenu extends Component{
+import Loader from 'react-loader-spinner'
 
-    openNav() {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        document.getElementById("openbtn").style.display = "none";
-      }
-      
-    closeNav() {
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft= "0";
-        document.getElementById("openbtn").style.display = "block";
-      }
+
+class Getdata extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            posts: [],
+            loading: true
+        }
+    }
+   // D:\myreactapps\reactyoutube\src\spinner.gif
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response =>{
+            console.log(response);
+            this.setState({posts: response.data});
+            this.setState({loading: false});
+            //this.setState({loading: true});
+            
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
 
 
     render(){
+       // const {posts} = this.state;
+       //const { this.state.posts, loading } = this.state;
+       let posts;
+       if(this.state.loading){
+           posts = <div className="tc">
+                {/*<img src={ loading } />*/}
+                <Loader
+         type="Puff"
+         color="gold"
+         height={70}
+         width={70}
+         className="loader tc pt3"
+         //timeout={3000} //3 secs
+ 
+      />
+               </div>
+       }
+       else{
+           posts = <div><table>
+           <thead>
+               <tr>
+                   <th>DeviceId</th>
+                   <th>Title</th>
+                   <th>Body</th>
+               </tr>
+           </thead>
+           
+           <tbody>
+               {this.state.posts.map(
+                   data=><tr key={data.id}>
+                           <td>{data.id}</td>
+                           <td>{data.title}</td>
+                           <td>{data.body}</td>
+                   </tr>
+               )}
+           </tbody>
+           
+       </table>
+       </div>
+       
+       }
         return(
-            <div className="font tc">
-                <div id="mySidebar" className="sidebar tl">
-                    <Link to="#" className="closebtn" onClick={()=>this.closeNav()}>×</Link>
-                    <Link className="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib white mw5" to="/">DashBoard</Link>
-                    <Link className="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib white mw5" to="./Aboutus">AboutUs</Link>
-                    <Link className="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib white mw5" to="./Demo">Demo</Link>
-                    <Link className="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib white mw5" to="./Charts">Charts</Link>
-                    <Link className="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib white mw5" to="./GetData">Data</Link>
-                    <Link className="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib white mw5" to="/Subscribe">Subscribe</Link>
-                </div>
-
-                <div id="main">
-                <button className="openbtn" id="openbtn" onClick={()=>this.openNav()}>&#9776;</button>
-                </div>
+            <div className="bg-white">
+                {posts}
             </div>
+            
         )
     }
 }
 
-export default SideMenu;
+export default Getdata;
